7cc6373c60123da822ed62ffabb189ee60a52377
nireekshavs <you@example.com>
2024-05-09 08:08:23 +0530

    added files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cnnClassifier',
+    version= '0.0.0',
+    author= 'Nireeksha',
+    author_email= 'nireekshavshetty@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
